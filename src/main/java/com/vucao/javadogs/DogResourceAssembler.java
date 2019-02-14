2267be55a8a4072d33e8bdcc7c3de2a5f632f5f8
Vu Cao <cdhvu@yahoo.com>
2019-02-13 22:36:20 -0800

    Adding DogResourceAssembler to manage data returned from database

---
+++ src/main/java/com/vucao/javadogs/DogResourceAssembler.java
@@ -0,0 +1,21 @@
+package com.vucao.javadogs;
+
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+@Component
+public class DogResourceAssembler implements ResourceAssembler<Dog, Resource<Dog>>
+{
+    @Override
+    public Resource<Dog> toResource(Dog dog)
+    {
+        return new Resource<Dog>(dog,
+                linkTo(methodOn(DogController.class).findOne(dog.getId())).withSelfRel(),
+                linkTo(methodOn(DogController.class).all()).withRel("dogs")
+        );
+    }
+}
